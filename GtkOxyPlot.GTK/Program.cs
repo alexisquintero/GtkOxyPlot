389e68055131f539a32a0cb783cf189d4046671a
Alexis Quintero <alexis_quintero@hotmail.com.ar>
2018-11-25 14:54:01 -0300

    Add OxyPlot Graph

--- GtkOxyPlot.GTK/Program.cs
+++ GtkOxyPlot.GTK/Program.cs
@@ -1,25 +1,42 @@
 ﻿using System;
-using Xamarin.Forms;
-using Xamarin.Forms.Platform.GTK;
+using Gtk;
+using OxyPlot;
+using OxyPlot.GtkSharp;
+using OxyPlot.Series;
 
 namespace GtkOxyPlot.GTK
 {
   public class MainClass
   {
     [STAThread]
-    public static void Main(string[] args)
+    public static void Main()
     {
-      Gtk.Application.Init();
-      Forms.Init();
+      Application.Init();
 
-      //var app = new App();
-      var window = new FormsWindow();
+      Window myWin = new Window("My first Gtk# Application");
+      myWin.Resize(200, 200);
 
-      //window.LoadApplication(app);
-      window.SetApplicationTitle("Game of life");
-      window.Show();
+      Label myLbl = new Label
+      {
+        Text = "Hello World"
+      };
 
-      Gtk.Application.Run();
+      //myWin.Add(myLbl);
+      var plotModel = new PlotModel {
+        Title = "Example 1"
+      };
+
+      plotModel.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
+
+      var plotView = new PlotView {
+        Model = plotModel
+      };
+
+      myWin.Add(plotView);
+
+      myWin.ShowAll();
+
+      Application.Run();
     }
   }
 }
